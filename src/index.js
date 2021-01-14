8815cd6174cf00663be1324fe8fef9d48a46d269
darcy_zheng <darcy_joven@live.com>
2021-01-14 21:05:36 +0800

    tets

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom'; 
-import {Index} from './layout'
- 
+import ReactDOM from 'react-dom';
+import { Index } from './layout'
+import { StoreDemo } from './demodata'
+
+const Context = React.createContext(StoreDemo)
+
+const Provider = (props) => {
+  const [state,dispatch] = React.useReducer(reducer,)
+  return (
+    <Context.Provider values={ }>
+      {props.children}
+    </Context.Provider>
+  )
+}
 
 ReactDOM.render(
-  <Index/>
-  ,
+  <Index />,
   document.getElementById('root')
 );
 
- 
